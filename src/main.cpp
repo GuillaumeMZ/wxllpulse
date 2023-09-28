aa0d31b6377494c819a94061e4720ea185dd432e
GuillaumeMZ <58004135+GuillaumeMZ@users.noreply.github.com>
2023-09-28 15:29:48 +0200

    SDL2 setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,35 @@
 #include <iostream>
 
-int main() {
-    std::cout << "hello\n";
+#include <SDL.h>
+
+constexpr auto WXLLPULSE_INITIALISATION_FLAGS = SDL_INIT_AUDIO | SDL_INIT_VIDEO | SDL_INIT_EVENTS;
+
+int main()
+{
+    if(SDL_InitSubSystem(WXLLPULSE_INITIALISATION_FLAGS) != 0)
+    {
+        //todo: log
+        std::exit(1);
+    }
+
+    auto window = SDL_CreateWindow("Test",
+                     SDL_WINDOWPOS_UNDEFINED,
+                     SDL_WINDOWPOS_UNDEFINED,
+                     800,
+                     600,
+                     0);
+
+    SDL_Event event;
+    while(true)
+    {
+        SDL_PollEvent(&event);
+        if(event.type == SDL_QUIT)
+        {
+            break;
+        }
+    }
+
+    SDL_DestroyWindow(window);
+
+    SDL_QuitSubSystem(WXLLPULSE_INITIALISATION_FLAGS);
 }
--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,10 @@
+add_requires("libsdl")
+
 target("wxllpulse")
     set_default(true)
     set_kind("binary")
     set_languages("cxx17")
 
     add_files("src/**.cpp")
+    add_links("X11")
+    add_packages("libsdl")
